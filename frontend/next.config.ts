cd8fc8184bf1abe66b80eb4528a07d336414d659
dipansilwal <dipansilwal10@gmail.com>
2025-09-02 23:08:47 +0545

    changes:cloudflare

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/Laxmi-Shree-Investment-Website',
+  assetPrefix: '/Laxmi-Shree-Investment-Website',
+  eslint: { ignoreDuringBuilds: true }, // optional
 };
 
 module.exports = nextConfig;
