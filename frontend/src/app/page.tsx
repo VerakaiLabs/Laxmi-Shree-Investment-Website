3d360e47619dedbd4ca0d941fb5db1383a0c7c4f
dipansilwal <dipansilwal10@gmail.com>
2025-09-29 22:30:27 +0545

    fixed case

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
-import HeroPage from "./sections/hero/hero"
+import HeroPage from "./sections/Hero/hero"
 import AboutUs from "./sections/about/about"
 import ServicesPage from "./sections/services"
 import ContactCTASection from "./sections/contactCTA"
 import VisionPage from "./sections/visionPage"
 import Navbar from "./components/Navbar"
-import LogoCarousel from "./sections/carousel/carousel"
-import FounderCard from "./components/founderMessageCard"
-import MessageFromOurFounderPage from "./sections/founderMessage/founderMessagePage"
+import LogoCarousel from "./sections/Carousel/carousel"
+import MessageFromOurFounderPage from "./sections/FounderMessage/founderMessagePage"
 import InvestmentsSection from "./sections/Investment/investmentSection"
 
 export const runtime = "edge";
