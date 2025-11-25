9ceb5ac0846486b468d3188a017397501750d605
dipansilwal <dipansilwal10@gmail.com>
2025-11-25 10:16:48 +0545

    updated sitemap

---
+++ frontend/src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.laxmishreeinvestment.com/',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.laxmishreeinvestment.com/Gallery',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },{
+      url: 'https://www.laxmishreeinvestment.com/Notices',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.laxmishreeinvestment.com/Investments',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.laxmishreeinvestment.com/team',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.laxmishreeinvestment.com/Contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    
+  ];
+}
